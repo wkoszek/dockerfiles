20f8c1039af8969c5ad3a59fec680f9d3955cc82
Wojciech Adam Koszek <wojciech@koszek.com>
2016-06-22 23:24:08 -0700

    Bring WIP to the repo. To slow to build on MBA.

--- wkoszek/me/entrypoint.sh
+++ wkoszek/me/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
+set -e
+
+export NO_HEADLESS=yes
+export DISPLAY=":99.0"
+#sh -e /etc/init.d/xvfb start
+
 echo "# entrypoint"
+make bootstrap
+make doit pub
+
+
+#sleep 3 # give xvfb some time to start
+#openssl aes-256-cbc -K $encrypted_d99751a86d14_key -iv $encrypted_d99751a86d14_iv -in etc/deploy.enc -out $HOME/deploy -d
+#- chmod 600 $HOME/deploy
+#
+#git config --global user.email "wojciech@koszek.com"
+#git config --global user.name "Wojciech A. Koszek"
+#git remote add github git@github.com:wkoszek/me.git
+#cp etc/ssh_config ~/.ssh/config
+
