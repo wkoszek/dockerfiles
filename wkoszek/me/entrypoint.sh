1c838940a98278c6848ddfc1b1d45749b7eaf1f8
Wojciech Adam Koszek <wojciech@koszek.com>
2017-02-27 21:53:09 -0800

    Refactor the image
    Make sure you only get /me when the volume is mounted,
    and that we don't do an additional clone. It just takes
    too long and causes issues. Also just fetch 2 files (makefile, Gemfile)
    while building the image.

--- wkoszek/me/entrypoint.sh
+++ wkoszek/me/entrypoint.sh
@@ -5,6 +5,10 @@ export LANG=en_US.UTF-8
 
 set -e
 
+echo "# entrypoint"
+pwd
+ls -la
+
 #export NO_HEADLESS=yes
 export DISPLAY=":99.0"
 #sh -e /etc/init.d/xvfb start
@@ -21,14 +25,6 @@ mkdir  -p ~/.ssh
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 cp /me/etc/ssh_config ~/.ssh/config
 
-echo "# entrypoint"
-pwd
-ls -la
-#make bootstrap | ts
 make doit pub | ts
 echo $?
 echo "# finished"
-
-
-#sleep 3 # give xvfb some time to start
-#openssl aes-256-cbc -K $encrypted_d99751a86d14_key -iv $encrypted_d99751a86d14_iv -in etc/deploy.enc -out $HOME/deploy -d
